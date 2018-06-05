499ff1f6a887b0ccb3e7c2678da953fbc4385033
Dima Manyahin <dima.manyahin@skylum.com>
2018-06-05 10:56:59 +0300

    Initial Commit

---
+++ BezierPaths/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BezierPaths
+//
+//  Created by Dima Manyahin on 6/5/18.
+//  Copyright © 2018 Dima Manyahin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BezierPaths/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BezierPaths
+//
+//  Created by Dima Manyahin on 6/5/18.
+//  Copyright © 2018 Dima Manyahin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
